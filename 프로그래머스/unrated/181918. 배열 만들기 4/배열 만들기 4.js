11585ad2cbe0183f451cb5042ecafff3761c3737
원종석 <63568239+rebi13@users.noreply.github.com>
2023-10-25 00:16:43 +0900

    [level unrated] Title: 배열 만들기 4, Time: 4.81 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.js
@@ -0,0 +1,21 @@
+function solution(arr) {
+    let stk = [];
+    let i = 0;
+    while(i < arr.length) {
+        if(stk.length === 0) {
+            stk.push(arr[i]); 
+            ++i;
+        }
+        else {
+            if(stk.at(-1) < arr[i]) {
+                stk.push(arr[i]); 
+                ++i;
+            }
+            else if (stk.at(-1) >= arr[i]){
+                stk.pop();
+            }
+        }
+    }
+  
+    return stk;
+}
