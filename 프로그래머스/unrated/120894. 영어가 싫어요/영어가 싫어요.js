71f65cbc86ae33823449ab03319cb7572119b0be
원종석 <63568239+rebi13@users.noreply.github.com>
2023-10-29 23:58:22 +0900

    [level unrated] Title: 영어가 싫어요, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,9 @@
+function solution(numbers) {
+    const strNumber = ["zero", "one", "two", "three", "four", "five",
+                 'six', "seven", "eight", "nine"]
+    
+      strNumber.forEach((nums, index) => {
+        numbers = numbers.split(nums).join(index)}) 
+
+      return Number(numbers);
+}
