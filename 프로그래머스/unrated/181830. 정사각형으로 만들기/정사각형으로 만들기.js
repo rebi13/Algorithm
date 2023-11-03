9ba8e62eb0c1d60558f117f74a44d2872636671c
원종석 <63568239+rebi13@users.noreply.github.com>
2023-11-03 21:10:07 +0900

    [level unrated] Title: 정사각형으로 만들기, Time: 0.07 ms, Memory: 38.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181830. 정사각형으로 만들기/정사각형으로 만들기.js
@@ -0,0 +1,19 @@
+function solution(arr) {
+    const [x, y] = [arr.length, arr[0].length];
+    if(x === y) return arr;
+    else if (x > y) {
+        for(let i=0; i<arr.length; ++i) {
+            for(let j=0; j<x-y; ++j) {
+                arr[i].push(0);
+            }
+        }
+        console.log('a');
+    }
+    else {
+        for(let i=0; i<y-x; ++i) {
+            arr.push(Array(y).fill(0));
+        }
+    }
+    
+    return arr;
+}
