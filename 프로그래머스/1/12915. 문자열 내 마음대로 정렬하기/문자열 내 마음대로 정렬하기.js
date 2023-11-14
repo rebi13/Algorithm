ebaa727d376ab5e471a62a74d975c9cfc3292343
원종석 <63568239+rebi13@users.noreply.github.com>
2023-11-15 01:48:12 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,9 @@
+function solution(strings, n) {
+    strings.sort((a,b)=>{
+        if(a[n] !== b[n]){
+            return a[n] > b[n] ? 1 : -1
+        }
+        return a > b ? 1 : -1
+    })
+    return strings;
+}
